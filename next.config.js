c83aefd4e5979dbf37693c7ae6478402fe175b97
berkaysaahin <berkaynihas@gmail.com>
2023-12-27 14:58:42 +0300

    first commit

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  env: {
+    API_URL: "http://localhost:3000",
+    DB_URI: "mongodb://localhost:27017/ptech",
+    NEXTAUTH_SECRET: "codingwithabbas",
+  },
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+};
+
+module.exports = nextConfig;
