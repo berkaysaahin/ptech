6c3ed10551ea49e53be98656aa274c12478b6848
berkaysaahin <berkaynihas@gmail.com>
2023-12-27 19:28:02 +0300

    cart actions

--- next.config.js
+++ next.config.js
@@ -6,10 +6,11 @@ const nextConfig = {
   env: {
     API_URL: "http://localhost:3000",
     DB_URI: "mongodb://localhost:27017/ptech",
-    NEXTAUTH_SECRET: "codingwithabbas",
+   
   },
   images: {
     domains: ["res.cloudinary.com"],
+    domains: ['m.media-amazon.com']
   },
 };
 
