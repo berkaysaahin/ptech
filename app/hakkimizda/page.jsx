8596b91234f8eaf933e9da8b0972e14500cd7030
Gden1z <91488638+Gden1z@users.noreply.github.com>
2024-01-01 13:59:24 +0300

    Add files via upload

---
+++ app/hakkimizda/page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Hakkimizda from "@/components/hakkimizda/Hakkimizda";
+
+const HakkimizdaPage = () => {
+  return <Hakkimizda />;
+};
+
+export default HakkimizdaPage;
